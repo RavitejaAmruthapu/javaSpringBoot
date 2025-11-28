b2ab7370075158b7dfa4de7da089c01a80a912dd
RavitejaAmruthapu <ateja0@gmail.com>
2025-11-28 22:12:33 +0530

    added extra message

--- src/main/java/com/mkyong/StartApplication.java
+++ src/main/java/com/mkyong/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Docker + Spring Boot");
+        model.addAttribute("title", "Docker + Spring Boot with AI Devops");
         model.addAttribute("msg", "Welcome to the docker container platforms!");
         return "index";
     }
